29824e43e50edfefbf647abebbf5558884a8d217
Gerhard Lazu <gerhard@lazu.co.uk>
2013-05-17 13:23:37 +0100

    When converting back to a hash, leave empty values out

--- spec/lib/hstruct_spec.rb
+++ spec/lib/hstruct_spec.rb
@@ -2,11 +2,9 @@ require_relative '../spec_helper'
 
 require 'hstruct'
 
-Person = HStruct.new(:first_name, :last_name)
+Person = HStruct.new(:first_name, :last_name, :dob)
 
 describe HStruct do
-  let(:person) {  }
-
   it "is a Struct" do
     Person.ancestors.should include Struct
   end
@@ -17,7 +15,7 @@ describe HStruct do
     person.last_name.should be_nil
   end
 
-  it "can be converted back to a hash" do
+  it "when converting back to a hash, leaves empty values out" do
     person = Person.new(:first_name => "Jimmy")
     person.last_name = "Kiel"
     person.to_hash.should eql Hash[:first_name => "Jimmy", :last_name => "Kiel"]
