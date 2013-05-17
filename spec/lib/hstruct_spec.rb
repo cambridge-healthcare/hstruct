b7d33e5421aa216debc071261019cace777c2405
Gerhard Lazu <gerhard@lazu.co.uk>
2013-05-17 17:36:14 +0100

    I was trying to remember (but failed) about rspec's its earlier

--- spec/lib/hstruct_spec.rb
+++ spec/lib/hstruct_spec.rb
@@ -4,7 +4,7 @@ require 'hstruct'
 
 Person = HStruct.new(:first_name, :last_name, :dob)
 
-describe HStruct do
+describe "Person defined from an HStruct" do
   it "is a Struct" do
     Person.ancestors.should include Struct
   end
@@ -13,16 +13,17 @@ describe HStruct do
     expect { Person.new }.to_not raise_error(ArgumentError)
   end
 
-  it "can be instantiated from hash" do
-    person = Person.new(:first_name => "Jimmy")
-    person.first_name.should eql "Jimmy"
-    person.last_name.should be_nil
-  end
+  describe "instantiated from a hash" do
+    subject { Person.new(:first_name => "Jimmy") }
 
-  it "when converting back to a hash, leaves empty values out" do
-    person = Person.new(:first_name => "Jimmy")
-    person.last_name = "Kiel"
-    person.to_hash.should eql Hash[:first_name => "Jimmy", :last_name => "Kiel"]
+    its(:first_name) { should eql "Jimmy" }
+
+    its(:last_name) { should be_nil }
+
+    it "when converting back to a hash, leaves empty values out" do
+      subject.last_name = "Kiel"
+      subject.to_hash.should eql Hash[:first_name => "Jimmy", :last_name => "Kiel"]
+    end
   end
 end
 
