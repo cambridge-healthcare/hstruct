f2bc443f0388f768f9f14b725a4ade8c91525442
Gerhard Lazu <gerhard@lazu.co.uk>
2013-05-23 23:35:52 +0100

    Sandi Metz will be sad if I pass more than 4 arguments
    http://robots.thoughtbot.com/post/50655960596/sandi-metz-rules-for-developers

--- benchmarks/hstruct.rb
+++ benchmarks/hstruct.rb
@@ -5,27 +5,26 @@ require 'pry'
 require 'ostruct'
 
 # Struct
-PlainStruct = Struct.new(:foo, :bar, :baz, :qux, :quux)
+PlainStruct = Struct.new(:foo, :bar, :baz, :qux)
 
 # HStruct
 require_relative '../lib/hstruct'
-PlainHStruct = HStruct.new(:foo, :bar, :baz, :qux, :quux)
+PlainHStruct = HStruct.new(:foo, :bar, :baz, :qux)
 
 # Class
 class SimpleClass
-  attr_reader :foo, :bar, :baz, :qux, :quux
+  attr_reader :foo, :bar, :baz, :qux
 
-  def initialize(foo, bar, baz, qux, quux)
+  def initialize(foo, bar, baz, qux)
     @foo = foo
     @bar = bar
     @baz = baz
     @qux = qux
-    @quux = quux
   end
 end
 
 class CleverClass
-  attr_reader :foo, :bar, :baz, :qux, :quux
+  attr_reader :foo, :bar, :baz, :qux
 
   def initialize(args)
     args.each { |k, v| instance_variable_set("@#{k}", v) }
@@ -33,14 +32,13 @@ class CleverClass
 end
 
 class HashArgsClass
-  attr_reader :foo, :bar, :baz, :qux, :quux
+  attr_reader :foo, :bar, :baz, :qux
 
   def initialize(args)
     @foo  = args[:foo]
     @bar  = args[:bar]
     @baz  = args[:baz]
     @qux  = args[:qux]
-    @quux = args[:quux]
   end
 end
 
@@ -49,63 +47,61 @@ hash = {
   :bar  => 2,
   :baz  => 3,
   :qux  => 4,
-  :quux => 5,
 }
 
-# All commented benchmarks were run on:
+# All benchmarks were run on:
 # * ruby 1.9.3p392 (2013-02-22 revision 39386) [x86_64-darwin12.3.0]
 # * Intel i7 2.2Ghz (MBP 8,2)
 
 runs = ENV.fetch('RUNS', 5).to_i
 
 Benchmark.ips(runs) do |x|
-  # 10k/s
+  # 14k/s
   x.report('OpenStruct') do
     OpenStruct.new(hash)
   end
 
-  # 65k/s
+  # 80k/s
   x.report('Class Clever') do
     CleverClass.new(hash)
   end
 
-  # 170k/s
+  # 185k/s
   x.report('HStruct') do
     PlainHStruct.new(hash)
   end
 
-  # 325k/s
+  # 380k/s
   x.report('Hash') do
     hash = {
       :foo  => 1,
       :bar  => 2,
       :baz  => 3,
       :qux  => 4,
-      :quux => 5,
     }
   end
 
-  # 350k/s
+  # 385k/s
   x.report('Class Hash Args') do
     HashArgsClass.new(hash)
   end
 
-  # 450k/s
+  # 470k/s
   x.report('Class Plain') do
-    SimpleClass.new(1, 2, 3, 4, 5)
+    SimpleClass.new(1, 2, 3, 4)
   end
 
-  # 600k/s
+  # 620k/s
   x.report('Struct') do
-    PlainStruct.new(1, 2, 3, 4, 5)
+    PlainStruct.new(1, 2, 3, 4)
   end
 end
 
 ### This is how some of the more popular gems compare to the above:
 #
 # Virtus         => 12k/s (no coercions) & 17k/s (coercions)
-# Hashr          => 21k/s
+# Hashr          => 20k/s
 # FastOpenStruct => 30k/s
-# Hashie::Dash   => 34k/s
-# Hashie MI & MA => 98k/s
+# Hashie::Dash   => 35k/s
+# Hashie MI & MA => 100k/s
 
