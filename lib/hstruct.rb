60380d0da30b8b31726cd4c025e39841ffe467d0
Stephen Best <bestie@gmail.com>
2013-06-07 14:09:45 +0100

    Support hashes with string keys
    This is acheived by sybolizing (with #to_sym) on initialize.
    Only occurs if the FIRST hash key is not a symbol (preserving performance).
    Support could potentially extend to any object that implements #to_sym.

--- lib/hstruct.rb
+++ lib/hstruct.rb
@@ -1,6 +1,14 @@
 class HStruct < Struct
-  def initialize(args = nil)
-    super(*members.map { |m| args[m] }) if args
+  def initialize(args = {})
+    raise ArgumentError unless args.is_a?(Enumerable)
+
+    unless args.keys.first.is_a?(Symbol)
+      args = args.each_with_object({}) do |(k,v), result|
+        result[k.to_sym] = v
+      end
+    end
+
+    super(*members.map { |m| args[m] })
   end
 
   def to_h
--- test/lib/hstruct_test.rb
+++ test/lib/hstruct_test.rb
@@ -1,6 +1,7 @@
 require_relative '../test_helper'
 
 require 'hstruct'
+require 'pry'
 
 Person = HStruct.new(:first_name, :last_name)
 
@@ -13,18 +14,48 @@ describe "Person defined from an HStruct" do
     Person.new.must_be_instance_of Person
   end
 
-  describe "instantiated from a hash" do
-    let(:person) { Person.new(:first_name => "Jimmy") }
+  it "argument must be enumerable" do
+    not_enumerable = Object.new
 
-    it "first name" do
-      person.first_name.must_equal "Jimmy"
+    ->{ Person.new(not_enumerable) }.must_raise ArgumentError
+  end
+
+  describe "instantiated from a hash" do
+    describe "with Symbol keys" do
+      let(:person_hash) { {
+        :first_name => "Jimmy",
+      } }
+
+      let(:person) { Person.new(person_hash) }
+
+      it "first name" do
+        person.first_name.must_equal "Jimmy"
+      end
+
+      it "last name" do
+        person.last_name.must_equal nil
+      end
     end
 
-    it "last name" do
-      person.last_name.must_equal nil
+    describe "with String keys" do
+      let(:person_hash) { {
+        "first_name" => "Alex",
+      } }
+
+      let(:person) { Person.new(person_hash) }
+
+      it "first name" do
+        person.first_name.must_equal "Alex"
+      end
+
+      it "last name" do
+        person.last_name.must_equal nil
+      end
     end
 
     describe "when converting back to a hash" do
+      let(:person) { Person.new( :first_name => "Jimmy" ) }
+
       it "includes empty values" do
         person.to_h.must_equal Hash[:first_name => "Jimmy", :last_name => nil]
       end
