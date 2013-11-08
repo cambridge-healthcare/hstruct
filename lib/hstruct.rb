a40630981e3272b1a3730bec3d7506576ed47d85
Camille Baldock <camille@camillebaldock.co.uk>
2013-11-08 13:35:04 +0000

    Add update method to HStruct

--- lib/hstruct.rb
+++ lib/hstruct.rb
@@ -1,7 +1,6 @@
 class HStruct < Struct
   def initialize(args = {})
     raise ArgumentError unless args.is_a?(Enumerable)
-
     unless args.keys.first.is_a?(Symbol)
       args = args.each_with_object({}) do |(k,v), result|
         result[k.to_sym] = v
@@ -13,5 +12,14 @@ class HStruct < Struct
 
   def to_h
     Hash[each_pair.to_a]
+  end
+
+  def update(args={})
+    result = self.to_h
+    raise ArgumentError unless args.is_a?(Enumerable)
+    args.each do |(k,v)|
+      result[k.to_sym] = v
+    end
+    self.class.new(result)
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-
+$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "lib")
 require 'minitest/autorun'
 require "minitest/reporters"
 MiniTest::Reporters.use! MiniTest::Reporters::SpecReporter.new
